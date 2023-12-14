f6a4f1e9f89a703f3bff64cdfe7399505f2b24f6
Preprocess <hello@preprocess.co>
2023-12-14 17:00:06 +0200

    Add integration test for testing preprocess loader

---
+++ libs/langchain/tests/integration_tests/preprocess/test_preprocess.py
@@ -0,0 +1,20 @@
+import os
+
+# from llama_index import download_loader
+from langchain.document_loaders import PreprocessLoader
+from langchain_core.documents import Document
+
+API_KEY = (
+    ""  # you've to contact support@preprocess.co for generating an api key for you...
+)
+
+def test_preprocess_load_document():
+    filepath = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), "preprocess_test.pdf"
+    )
+    loader = PreprocessLoader(api_key=API_KEY, filepath=filepath)
+    documents = loader.load_data()
+
+    assert isinstance(documents, list)
+    assert all(isinstance(doc, Document) for doc in documents)
+    assert all(doc.text is not None for doc in documents)

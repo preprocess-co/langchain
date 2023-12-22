44ec3d9b5c33adc97ff7039494e05f153e23fd79
Preprocess <hello@preprocess.co>
2023-12-22 19:53:20 +0200

    Update preprocess files.

--- libs/community/langchain_community/document_loaders/preprocess.py
+++ libs/community/langchain_community/document_loaders/preprocess.py
@@ -1,4 +1,4 @@
-"""Preprocess Reader."""
+"""Preprocess Loader."""
 import os
 from typing import List
 
@@ -10,21 +10,26 @@ from langchain_community.document_loaders.base import BaseLoader
 class PreprocessLoader(BaseLoader):
     def __init__(self, path, *args, **kwargs):
         """
-        Preprocess is an API service that splits any kind of document into
-        optimal chunks of text for use in language model tasks. Given documents
-        as input Preprocess splits them into chunks of text that respect the layout
-        and semantics of the original document.
-        We split the content by taking into account sections, paragraphs,
-        lists, images, data tables, text tables, and slides
-        and following the content semantics for long texts.
-        We support:
+        Preprocess is an API service that splits documents into optimal chunks
+        of text for use in language model tasks.
+        It accepts various document formats as input and segments them into chunks
+        that respect the original document's layout and semantics.
+        This segmentation considers sections, paragraphs, lists, images,
+        data tables, text tables, and slides,
+        all while adhering to the semantics of lengthy texts.
+        Supported formats include:
             - PDFs
             - Microsoft Office documents (Word, PowerPoint, Excel)
             - OpenOffice documents (ods, odt, odp)
             - HTML content (web pages, articles, emails)
             - Plain text
-        You need a Preprocess API Key to use the SDK,
-        to get one please reach out to support@preprocess.co asking for an API key.
+
+        You need a Preprocess API Key, to get one please reach out to
+        support@preprocess.co
+
+        You can instantiate the loader by passing the file path.
+        If you wish to load already chunked files,
+        this can be done by providing the process_id to the loader.
         """
         try:
             from pypreprocess import Preprocess
@@ -37,10 +42,7 @@ class PreprocessLoader(BaseLoader):
         if "api_key" in kwargs:
             api_key = kwargs["api_key"]
             if api_key is None or api_key == "":
-                raise ValueError(
-                    "Please provide an api key to be used while"
-                    + "doing the auth with the system."
-                )
+                raise ValueError("Please provide a valid api key")
         del kwargs["api_key"]
 
         _options = {}
@@ -79,19 +81,22 @@ class PreprocessLoader(BaseLoader):
         # Remember to path either filepath or process_id,
         # both of them can't be None or not passed
         if self._filepath is None and self._process_id is None:
-            raise ValueError(
-                "Please provide either filepath or process_id to handle the resutls."
-            )
+            raise ValueError("Please provide filepath or process_id.")
 
         self._chunks = None
 
     def load(self, return_whole_document=False) -> List[Document]:
         """
-        This function loads to you the chunks from preprocess sdk
-        and return it to you. If you want to return only the extracted text
-        and handle it with custom pipelines set `return_whole_document = True`
-        Otherwise this function will return to you a list of Documents each one
-        containing a chunk with metadata of filename
+        Retrieve document chunks for downstream use in generating embeddings.
+        Preprocess automatically generates optimal text chunks based
+        on the original document's layout and content semantics.
+        Applying TextSplitters to Documents returned by this method is not recommended.
+
+        If you want to obtain only the extracted text for processing with custom
+        Document transformers, set return_whole_document to True.
+
+        This function returns a list of Documents, each containing a chunk.
+        The filename is included in the metadata.
         """
         if self._chunks is None:
             if self._process_id is not None:
@@ -140,8 +145,8 @@ class PreprocessLoader(BaseLoader):
 
     def get_process_id(self):
         """
-        This function you can call it to give you back the process_id
-        to store it as you want to use it later.
+        process_id allows you to initialize the loader
+        wihtout chunking the document again.
         """
         return self._process_id
 
